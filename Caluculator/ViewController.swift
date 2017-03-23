541a5dcc8a74451f19abe144b2621c6c1ccd207a
Brett Gordon <gordonbrett89@gmail.com>
2017-03-23 12:39:26 -0400

    Added operators

--- Caluculator/ViewController.swift
+++ Caluculator/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     var userIsTyping = false;
     
     @IBAction func touchDigit(_ sender: UIButton) {
@@ -27,9 +27,9 @@ class ViewController: UIViewController {
         
         if userIsTyping {
             let currentTextInDisplay = display!.text!;
-            display!.text = currentTextInDisplay + digit;
+            display.text = currentTextInDisplay + digit;
         } else {
-            display!.text = digit;
+            display.text = digit;
             userIsTyping = true;
         }
         // print("\(digit)")
@@ -38,6 +38,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        let mathematicalSymbol = sender.currentTitle!
+        userIsTyping = false
+        switch mathematicalSymbol {
+        case "π":
+            displayValue = Double.pi
+        case "√":
+            displayValue = sqrt(displayValue)
+        default:
+            break;
+            
+        }
+    }
+    
 }
 
